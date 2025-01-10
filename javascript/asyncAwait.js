178bf183c16e6378fb6284f09835f66fc41fb895
dhiviya <dhiviyachandrasekaran093@gmail.com>
2025-01-10 21:50:42 +0530

    added

---
+++ javascript/asyncAwait.js
@@ -0,0 +1,10 @@
+
+async function asAw(){
+    setTimeout(() => {
+        //return("inside timeout");
+    },5000)
+     return"this is async/await example"
+}
+asAw().then(function(output){
+   console.log(output)
+})
